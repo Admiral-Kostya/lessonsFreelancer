1678a04a8ccc9f50b611f136921c434900fc32ac
Kostiantyn Osypchuk <119530557+Admiral-Kostya@users.noreply.github.com>
2023-12-25 11:24:06 +0200

    Add files via upload

---
+++ Project-Interior-bug-fixed/js/script.js
@@ -0,0 +1,43 @@
+"use strict";
+//----------------------------------------- Menu Burger -----------------------------------------//
+const icon = document.querySelector(".icon-menu");
+icon.addEventListener("click", function () {
+  document.documentElement.classList.toggle("menu-open");
+});
+//----------------------------------------- Scroll Animation -----------------------------------------//
+const animItems = document.querySelectorAll(".anim-items");
+if (animItems.length > 0) {
+  window.addEventListener("scroll", animOnScroll);
+  function animOnScroll(params) {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+      if (
+        pageYOffset > animItemOffset - animItemPoint &&
+        pageYOffset < animItemOffset + animItemHeight
+      ) {
+        animItem.classList.add("_active");
+      } else {
+        animItem.classList.remove("_active");
+      }
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return {
+      top: rect.top + scrollTop,
+      left: rect.left + scrollLeft,
+    };
+  }
+  setTimeout(() => {
+    animOnScroll();
+  }, 500);
+}
